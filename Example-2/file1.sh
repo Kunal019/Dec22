b4f04e2500c883a57f1d639c4738e23a6c44595b
kunal019 <kunal01@gmail.com>
2022-12-22 08:14:49 +0530

    updated shell script

---
+++ Example-2/file1.sh
@@ -0,0 +1,33 @@
+list1=()
+function1(){
+        url=169.254.169.254/latest/meta-data
+        for i in $(curl -s $url)
+        do
+                if [[  `echo $i | grep -v "/"` && $? = 0 ]]
+                then
+                        
+                        list1+=($i)
+
+                else
+                        for j in $(curl -s $url/$i)
+                        do
+                                if [[ `echo $j | grep -v "/"` && $? = 0 ]]
+                                then
+                                        
+                                        list1+=($i$j)
+                                fi
+                        done
+
+
+
+
+                fi
+        done
+         }
+
+
+function1
+for item in ${list1[*]}
+do
+        echo $item: $(curl -s http://169.254.169.254/latest/meta-data/$item)
+done
